883bb072635e1b3f3226ba56e694d08b4920a577
Taekwan Park <techigh@Taekwans-MacBook-Pro.local>
2022-06-30 20:49:40 +0900

    -

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -8,5 +8,7 @@ const client =
   global.client || new PrismaClient({ log: ["info", "warn", "error"] });
 
 if (process.env.NODE_ENV === "development") global.client = client;
+console.log(global.client);
+console.log(client);
 
 export default client;
